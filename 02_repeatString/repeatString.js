776154f6141c67c853864d727add58edf6ce2245
Morauszki András <morauszki.andras@gmail.com>
2023-03-03 14:45:25 +0100

    exercise 1-7 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) return 'ERROR';
 
+  let finalString = '';
+
+  for (let i = 1; i <= num; i++) {
+    finalString += string;
+  }
+
+  return finalString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== 'number' ||
+    typeof num2 !== 'number' ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return 'ERROR';
 
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
+
+  let sum = 0;
+
+  for (let num = start; num <= end; num++) {
+    sum += num;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
