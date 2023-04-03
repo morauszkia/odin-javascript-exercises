44e9aec0322f2792a77e2a02a2ea8b6c8f1dd446
Morauszki András <morauszki.andras@gmail.com>
2023-04-03 21:25:47 +0200

    exercises 11&12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArr) {
+  const ages = peopleArr.map(
+    (person) => (person.yearOfDeath || 2023) - person.yearOfBirth
+  );
+  console.log(ages);
+  const oldestIdx = ages.findIndex((age) => age === Math.max(...ages));
+  console.log(oldestIdx);
 
+  return peopleArr[oldestIdx];
 };
 
 // Do not edit below this line
