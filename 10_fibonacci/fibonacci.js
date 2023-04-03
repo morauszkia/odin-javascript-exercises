f14c36bfbbb23259b9abaa7e81ca550aa6c9ddbb
Morauszki András <morauszki.andras@gmail.com>
2023-04-03 21:06:33 +0200

    exercise fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const number = parseInt(num);
+  if (number < 1) return 'OOPS';
 
+  if (number < 3) return 1;
+
+  return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
