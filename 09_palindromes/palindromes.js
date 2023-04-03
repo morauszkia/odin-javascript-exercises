bebcfb74cf8070f34f740d204df483eb8da081aa
Morauszki András <morauszki.andras@gmail.com>
2023-04-03 16:22:21 +0200

    exercise palindromes solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const lowerCasesLettersOnly = word.toLowerCase().replace(/[^a-z]/g, '');
 
+  return (
+    lowerCasesLettersOnly.split('').reverse().join('') === lowerCasesLettersOnly
+  );
 };
 
 // Do not edit below this line
